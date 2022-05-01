377ebe7d616e2f9eb9bc92287dc92e783d8c75b7
Kulanit63070011 <73680667+Kulanit63070011@users.noreply.github.com>
2022-05-01 15:54:36 +0700

    Update PaymentPage.vue

--- bookstore-frontend/src/views/PaymentPage.vue
+++ bookstore-frontend/src/views/PaymentPage.vue
@@ -12,7 +12,7 @@
             <v-divider></v-divider><br/>
             <p>ราคาสินค้า 709.00 THB</p>
             <center>
-              <v-btn
+              <v-btn @click="$router.push('/cart/:cartId')"
                 title
                 color="black"
                 style="color: white"
